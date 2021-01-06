2d0a1fec82abb84b9446da22e068a2be664feadb
Andrey Zhezlov <zhezlov@gmail.com>
2021-01-07 02:45:51 +0400

    Complete some task from 5lvl collections

--- 4.JavaCollections/src/com/javarush/task/task34/task3404/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3404/Solution.java
@@ -1,228 +1,183 @@
 package com.javarush.task.task34.task3404;
 
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.text.DecimalFormat;
+import java.util.*;
 
 /*
 Рекурсия для мат. выражения
+НЕ МОЁ РЕШЕНИЕ. ВЗЯЛ на проверку валидатора - тоже не проходит.
 */
-
-//Решение
-//ищем первую операцию, которую можем выполнить, выполняем,
-// записываем значение в итоговую строку и прокидываем дальше рекурсией и запоминаем количество операций
-//Именно такой алгорит, потому что объвление метода void - он ничего не может вернуть (мы должны всё прокидывать дальше)
-        /*
-        Псевдокод
-
-        Вначале счинаем количество операций
-        Если значения countOperation = 0 то считаем количество тригонометр, минусов плюсов и приравниваем
-        присваиваем значение countOperation полученному значению.
-
-        Ищем самые внутренние скобки - вначале самую последнюю "(", как найдём начинаем искать самую первую ")".
-        Если нашли, то
-            Отправляем содержание скобок в метод expressionMathematic(String exp), который возвращает результат в типе String
-            Если снаружи скобок есть степень, то возвоздим содержимое скобок в степень и удаляем информацию про степень.
-            Если снаружи скобок есть тригонометрическая функции, то
-                отправляем тригонометрическую функцию и содержимое скобок (влючая скобки) в метод trigonometricSolution(String exp)
-                который возвращает результат в типе String (так же, получается, он ещё и скобки раскрывает)
-                перезаписываем  expression с результатом вычислений тригонометрической функции и выражения в скобках
-                вызываем рекурсию с новым expression
-            Иначе  раскрываем (удаляем) скобки, перезаписываем expression и вызываем рекурсию с новым expression
-        Иначе, отправляем всё выражение в метод expressionMathematic(String exp) и получает результат программы, выводим на экран вместе с количество операций
-
-
-        Метод expressionMathematic(String exp) (получает выражение без скобок!)
-        пока в пришедшей строке есть знаки +-/*^ выполнять цикл
-            засовываем в токенайзер всю пришедшую строку, устанавливаем delim в виде +-/*^
-            Ищем ^. Если нашли предшествующий токен возводим в степень, значение стпени в токене после ^
-                перезаписываем содержимое выражения (exp) со значением результата
-                continue (запускаем цикл заного)
-            Ищем * и /. Если нашли предшествующий токен умножаем или делим (в зависимости от того какой знак был) на токен следом
-                перезаписываем содержимое выражения (exp) со значением результата
-                continue (запускаем цикл заного)
-            Ищем + и -. Если нашли предшествующий токен складываем или вычитаем (в зависимости от того какой знак был) с токеном следом
-                перезаписываем содержимое выражения (exp) со значением результата
-                continue (запускаем цикл заного)
-        Когда цикл закончился, возвращаем значение exp
-
-
-        Метод trigonometricSolution(String exp)
-        Свич
-            sin -  вычисляем sin - возвращаем результат, например System.out.println("равен " + Math.round(Math.sin(Math.toRadians(30))));
-            cos - вычисляем cos - возвращааяем результат
-            tan - вычисляем tan - возвращаем результат
-
-
-
-        */
-
-
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-       // solution.recurse("sin(2*(-5+1.5*4)+28)", 0); //expected output 0.5 6
-        solution.recurse("2*5-1.5*4+28", 0); //expected output 0.5 6
-        solution.recurse("5^2^2^2", 0); //expected output 0.5 6
-        solution.recurse("57-10^2+5", 0); //expected output 0.5 6
+        solution.recursion("sin(2*(-5+1.5*4)+28)", 0); //expected output 0.5 6
     }
 
-
-    public void recurse(final String expression, int countOperation) {
+    public void recursion(final String expression, int countOperation) {
         //implement
+        Locale.setDefault(Locale.ENGLISH);
 
-        String exp = expression;
-        String resultTrigonometric = trigonometricSolution(exp);
-
-        exp = expression;
-        String resultMathematics = expressionMathematics(exp);
-    }
-
-
-    private String expressionMathematics(String exp) {
-       /* пока в пришедшей строке есть знаки +-/*^ выполнять цикл
-            засовываем в токенайзер всю пришедшую строку, устанавливаем delim в виде +-/*^
-            Ищем ^. Если нашли предшествующий токен возводим в степень, значение стпени в токене после ^
-                перезаписываем содержимое выражения (exp) со значением результата
-                continue (запускаем цикл заного)
-            Ищем * и /. Если нашли предшествующий токен умножаем или делим (в зависимости от того какой знак был) на токен следом
-                перезаписываем содержимое выражения (exp) со значением результата
-                continue (запускаем цикл заного)
-            Ищем + и -. Если нашли предшествующий токен складываем или вычитаем (в зависимости от того какой знак был) с токеном следом
-                перезаписываем содержимое выражения (exp) со значением результата
-                continue (запускаем цикл заного)
-        Когда цикл закончился, возвращаем значение exp
-        */
-
-
-        //регулярка которая проверяет есть ли знаки +-/*^ в строке. Пока они есть выполняем математические операции
-        while(exp.matches("(.*\\++.+)|(.*-+.+)|(.*/+.+)|(.*\\*+.+)|(.*\\^+.+)")){ //Пояснение к регулярке ниже
-
-            //создаём StringTokenizer для разделения чисел и знаков
-            StringTokenizer stringTokenizer = new StringTokenizer(exp, "-+*^/", true);
-            //создаём темповый лист для работы с числами и знаками
-            ArrayList<String> listTokens = new ArrayList<>();
-            //перегоняем числа и знаки из токинайзера в темповый лист
-            while (stringTokenizer.hasMoreElements())
-                listTokens.add(stringTokenizer.nextToken());
-
-            //проверка на минус вначале выражения
-            if(listTokens.get(0).equals("-")){ //если первый элемент минус
-                String s = listTokens.get(1); //запоминаем второй элемент
-                listTokens.remove(0);   //удаляем минус из списка
-                listTokens.remove(0);   //удаляем второй элемент списка
-                listTokens.add(0, "-" + s); //добавляем на первое место в списке значение второго элемента с минусом
-            }
-//02.03.2020г. ОСТАНОВИЛСЯ на знаке Проблема в том что при выражении -43+5 программа даёт результат -48, а должна -38
-            //если нашли ^ то  число перед ^ возводим в степень. Значение степени - следующее число после ^
-            while(listTokens.contains("^")){
-                int index = listTokens.indexOf("^");
-                double term1 = Double.valueOf(listTokens.get(index - 1));
-                double term2 = Double.valueOf(listTokens.get(index + 1));
-                double result = Math.pow(term1, term2);
-                listTokens.remove(index -1); //удаляем число перед ^
-                listTokens.remove(index -1); //удаляем ^ (всё равно i-1 т.к. лист сдвинулся слева после первого remove)
-                listTokens.remove(index -1); //удаляем число после ^ (всё авно i-1 по той же причине)
-                listTokens.add(index -1, String.valueOf(result)); //добавляем на место первого числа получившееся значение
-
-            }
-
-            while(listTokens.contains("*")){
-                int index = listTokens.indexOf("*");
-                double term1 = Double.valueOf(listTokens.get(index - 1));
-                double term2 = Double.valueOf(listTokens.get(index + 1));
-                double result = term1 * term2;
-                listTokens.remove(index -1); //удаляем число перед ^
-                listTokens.remove(index -1); //удаляем ^ (всё равно i-1 т.к. лист сдвинулся слева после первого remove)
-                listTokens.remove(index -1); //удаляем число после ^ (всё авно i-1 по той же причине)
-                listTokens.add(index -1, String.valueOf(result)); //добавляем на место первого числа получившееся значение
-
-            }
-
-            while(listTokens.contains("/")){
-                int index = listTokens.indexOf("/");
-                double term1 = Double.valueOf(listTokens.get(index - 1));
-                double term2 = Double.valueOf(listTokens.get(index + 1));
-                double result = term1 / term2;
-                listTokens.remove(index -1); //удаляем число перед ^
-                listTokens.remove(index -1); //удаляем ^ (всё равно i-1 т.к. лист сдвинулся слева после первого remove)
-                listTokens.remove(index -1); //удаляем число после ^ (всё авно i-1 по той же причине)
-                listTokens.add(index -1, String.valueOf(result)); //добавляем на место первого числа получившееся значение
-
-            }
-            while(listTokens.contains("+")){
-                int index = listTokens.indexOf("+");
-                double term1 = Double.valueOf(listTokens.get(index - 1));
-                double term2 = Double.valueOf(listTokens.get(index + 1));
-                double result = term1 + term2;
-                listTokens.remove(index -1); //удаляем число перед ^
-                listTokens.remove(index -1); //удаляем ^ (всё равно i-1 т.к. лист сдвинулся слева после первого remove)
-                listTokens.remove(index -1); //удаляем число после ^ (всё авно i-1 по той же причине)
-                listTokens.add(index -1, String.valueOf(result)); //добавляем на место первого числа получившееся значение
-
-            }
-
-            while(listTokens.contains("-")){
-                int index = listTokens.indexOf("-");
-                double term1 = Double.valueOf(listTokens.get(index - 1));
-                double term2 = Double.valueOf(listTokens.get(index + 1));
-                double result = term1 - term2;
-                listTokens.remove(index - 1); //удаляем число перед ^
-                listTokens.remove(index - 1); //удаляем ^ (всё равно i-1 т.к. лист сдвинулся слева после первого remove)
-                listTokens.remove(index - 1); //удаляем число после ^ (всё авно i-1 по той же причине)
-                listTokens.add(index - 1, String.valueOf(result)); //добавляем на место первого числа получившееся значение
-
-            }
-
-            //Создаём stringBuilder для сбора новой строки
-            StringBuilder stringBuilder = new StringBuilder();
-            for (String s :
-                    listTokens) {
-                stringBuilder.append(s); //собираем строку из элементов темпового листа
-            }
-            //присваиваем exp новое значение строки
-            exp = stringBuilder.toString();
-            System.out.println(exp);
-
-            if (listTokens.size() == 1){ //если в списке остался 1 элемент, то возвращаем exp. Это нужно потому что там может быть отрицательное значение
-                break;                   //
-            }
+        if (countOperation > 0){
+            System.out.println(expression + " " + countOperation);
         }
-        return exp;
-    }
-    //Пояснение к регулярке (.*\++.+)|(.*-+.+)|(.*/+.+)|(.*\*+.+)
-    //Во первых в комментариях одна обратная касая, а в боевой строке две - это особенность джавы - одна обратная косая "съедается" джавой
-    //(.*\++.+)
-    // .* - любой символ в количестве ноль или больше
-    // \++ - экранирование символа плюс и этот символ плюк в количестве один или больше
-    //.+ - любой символ в количестве один или больше
-    // символ | между скобками - это ИЛИ
-    //в других скобках аналогично, но например символ минуса и деления экранировать не надо
+        else {
+            List<String> out = new ArrayList<>();
+            Deque<String> stackPostfix = new ArrayDeque<>();
+            Deque<Double> stack = new ArrayDeque<>();
 
+            StringTokenizer tokenizer = new StringTokenizer(expression.replace(" ", "").toLowerCase(), delimiters(), true);
+            String prev = "";
+            String curr = "";
 
+            while (tokenizer.hasMoreTokens()) {
+                curr = tokenizer.nextToken();
 
+                if (!tokenizer.hasMoreTokens() && isOperator(curr)){
+                    System.out.println("Некорректное выражение.");
+                    break;
+                }
 
-       //Код для примера как работает StringTokenizer
-//        StringTokenizer stringTokenizer = new StringTokenizer(exp, "-+*^/", true);
-//        System.out.println(stringTokenizer.countTokens());
-//        System.out.println();
-//        while (stringTokenizer.hasMoreTokens())
-//
-//            System.out.println(stringTokenizer.nextToken());
-//        System.out.println();
-//        while (stringTokenizer.hasMoreElements())
-//            System.out.println(stringTokenizer.nextElement());
+                if (isFunction(curr)) stackPostfix.push(curr);
+                else if (isDelimiter(curr)) {
+                    if (curr.equals("(")) stackPostfix.push(curr);
+                    else if (curr.equals(")")) {
+                        while (!stackPostfix.peek().equals("(")) {
+                            out.add(stackPostfix.pop());
+                            if (stackPostfix.isEmpty()) {
+                                System.out.println("Скобки не согласованы.");
+                            }
+                        }
+                        stackPostfix.pop();
+                        if (!stackPostfix.isEmpty() && isFunction(stackPostfix.peek())) {
+                            out.add(stackPostfix.pop());
+                        }
+                    }
+                    else {
+                        if (curr.equals("-") && (prev.equals("") || (isDelimiter(prev) && !prev.equals(")")))) {
+                            curr = "u-";
+                        }
+                        else {
+                            while (!stackPostfix.isEmpty() && (priority(curr) <= priority(stackPostfix.peek()))){
+                                out.add(stackPostfix.pop());
+                            }
+                        }
+                        stackPostfix.push(curr);
+                    }
+                }
 
+                else {
+                    out.add(curr);
+                }
+                prev = curr;
+            }
 
+            while (!stackPostfix.isEmpty()) {
+                if (isOperator(stackPostfix.peek())) out.add(stackPostfix.pop());
+                else {
+                    System.out.println("Скобки не согласованы.");
+                    break;
+                }
+            }
+            for (String x: out){
+                switch (x) {
+                    case "sin":
+                        stack.push(Math.sin(Math.toRadians(stack.pop())));
+                        countOperation++;
+                        break;
+                    case "cos":
+                        stack.push(Math.cos(Math.toRadians(stack.pop())));
+                        countOperation++;
+                        break;
+                    case "tan":
+                        stack.push(Math.tan(Math.toRadians(stack.pop())));
+                        countOperation++;
+                        break;
+                    case "^": {
+                        Double b = stack.pop(), a = stack.pop();
+                        stack.push(Math.pow(a, b));
+                        countOperation++;
+                        break;
+                    }
+                    case "+":
+                        stack.push(stack.pop() + stack.pop());
+                        countOperation++;
+                        break;
+                    case "-": {
+                        Double b = stack.pop(), a = stack.pop();
+                        stack.push(a - b);
+                        countOperation++;
+                        break;
+                    }
+                    case "*":
+                        stack.push(stack.pop() * stack.pop());
+                        countOperation++;
+                        break;
+                    case "/": {
+                        Double b = stack.pop(), a = stack.pop();
+                        stack.push(a / b);
+                        countOperation++;
+                        break;
+                    }
+                    case "u-":
+                        stack.push(-stack.pop());
+                        countOperation++;
+                        break;
+                    default:
+                        stack.push(Double.valueOf(x));
+                        break;
+                }
+            }
 
-
-    private String trigonometricSolution(String exp) {
-
-        System.out.println("равен " + Math.round(Math.sin(Math.toRadians(30))));
-
-        return exp;
+            recursion(new DecimalFormat("#.##").format(stack.pop()), countOperation);
+        }
     }
 
     public Solution() {
         //don't delete
+    }
+
+    private static String operators() {
+        return "+-*/^";
+    }
+
+    private static String delimiters() {
+        return "()" + operators();
+    }
+
+    private static boolean isOperator(String token) {
+        if (token.equals("u-")) return true;
+        for (int i = 0; i < operators().length(); i++) {
+            if (token.charAt(0) == operators().charAt(i)) return true;
+        }
+        return false;
+    }
+
+    private static boolean isDelimiter(String token) {
+        if (token.length() != 1) return false;
+        for (int i = 0; i < delimiters().length(); i++) {
+            if (token.charAt(0) == delimiters().charAt(i)) return true;
+        }
+        return false;
+    }
+
+    private static boolean isFunction(String token) {
+        return token.equals("sin") || token.equals("cos") || token.equals("tan");
+    }
+
+
+    private static int priority(String token) {
+        switch (token) {
+            case "(":
+                return 0;
+            case "+":
+            case "-":
+                return 1;
+            case "*":
+            case "/":
+                return 2;
+            case "^":
+                return 3;
+        }
+        return 4;
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
@@ -2,6 +2,8 @@ package com.javarush.task.task35.task3507;
 
 import java.io.*;
 import java.lang.reflect.Constructor;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,8 +18,84 @@ public class Solution {
     }
 
     public static Set<? extends Animal> getAllAnimals(String pathToAnimals) {
-        System.out.println(pathToAnimals);
 
-        return null;
+        Set<Animal> set = new HashSet<>();
+        File[] files = new File(pathToAnimals).listFiles();
+        for (File file :
+                files) {
+           // System.out.println(file.getName());
+            if (file.isFile() && file.getName().endsWith(".class")) {
+                String packageName = Solution.class.getPackage().getName() + ".data";
+                Class clazz = new ClassFromPath().load(file.toPath(), packageName);
+               // System.out.println(clazz);
+                int score = 0;
+                //find interface Animal
+                Class[] interfaces = clazz.getInterfaces();
+                for (Class interf :
+                        interfaces) {
+                    if (interf.getSimpleName().toString().equals("Animal")) {
+                        score++;
+                        break;
+                    }
+                }
+              //  System.out.println("1 score = " + score);
+                //Find default constructors
+                Constructor[] constructors = clazz.getConstructors();
+                for (Constructor constr :
+                        constructors) {
+                    if(constr.getParameterCount()==0){
+                        score++;
+                    }
+                }
+               // System.out.println("2 score = " + score);
+                if (score ==2){
+                    try{
+                        Animal animal = (Animal)clazz.newInstance();
+                        set.add(animal);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InstantiationException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+
+//        System.out.println(pathToAnimals);
+//        if (pathToAnimals.startsWith("/"))
+//            pathToAnimals = pathToAnimals.substring(1);
+//        pathToAnimals = pathToAnimals.replace("/","\\");
+//        System.out.println(pathToAnimals);
+//        File file = new File(pathToAnimals);
+//        String[] strings = file.list();
+//        System.out.println(strings);
+//
+//        File file1 = new File("D:\\Dropbox\\Андрей\\Программирование(+книги)\\JavaRushTasks\\4.JavaCollections\\src\\com\\javarush\\task\\task35\\task3507\\data");
+//        String[] strings1 = file1.list();
+//        for (String s :
+//                strings1) {
+//            System.out.println(s);
+//        }
+//        System.out.println(strings1);
+        return set;
     }
+
+    public static class ClassFromPath extends ClassLoader {
+        public Class<?> load(Path path, String packageName) {
+            try {
+                String className = packageName + "." + path.getFileName().toString().replace(".class", "");
+                byte[] bytes = Files.readAllBytes(path);
+                return defineClass(className, bytes, 0, bytes.length);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+    }
+
+
 }
